77ff93f494515ec23527aaa70604bf8d64c9e61c
salah <salahsaamir707@gmail.com>
2024-02-14 21:57:40 +0200

    delete wishlist store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,14 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./uiSlice";
 import { userReducer } from "./UserData.jsx";
 import cartSlice from "./cartSlice";
-import wishlistSlice from "./wishlistSlice.js"; // Import your wishlist slice
+// import wishlistSlice from "./wishlistSlice.js"; // Import your wishlist slice
 
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
     UserData: userReducer,
     cart: cartSlice.reducer,
-    wishlist: wishlistSlice.reducer, // Add your wishlist slice to the reducers
+    // wishlist: wishlistSlice.reducer, // Add your wishlist slice to the reducers
   },
 });
 
