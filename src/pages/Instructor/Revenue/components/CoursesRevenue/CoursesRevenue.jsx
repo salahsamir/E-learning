ca29ac5f982e5362969e6afa7b510d0d32267474
Osama Safwat <rovendix@gmail.com>
2024-03-22 21:50:28 +0200

    added courses revenue

---
+++ src/pages/Instructor/Revenue/components/CoursesRevenue/CoursesRevenue.jsx
@@ -0,0 +1,48 @@
+import { Box } from "@mui/material";
+import { useGetRevenue } from "api/instructor/revenue.tsx";
+import React from "react";
+import CustomTable from "shared/ui/CustomTable/CustomTable";
+
+const headCells = [
+  {
+    id: "courseName",
+    disablePadding: false,
+    label: "Course Name",
+  },
+  {
+    id: "totalSales",
+    disablePadding: false,
+    label: "Total Sales",
+  },
+  {
+    id: "totalRevenue",
+    disablePadding: false,
+    label: "Total Revenue",
+  },
+];
+const CoursesRevenue = () => {
+  const {
+    data: revenue,
+    isLoading: loadingCourses,
+    isError: errorCourses,
+  } = useGetRevenue();
+  console.log(revenue?.courses);
+  const rows = revenue?.courses?.map((course) => ({
+    id: course.courseId,
+    courseName: course.title,
+    totalSales: course.numberOfStudents,
+    totalRevenue: course.revenue,
+  }));
+  return (
+    <Box>
+      <CustomTable
+        headCells={headCells}
+        rows={rows || []}
+        loading={loadingCourses}
+        error={errorCourses}
+      />
+    </Box>
+  );
+};
+
+export default CoursesRevenue;
--- src/pages/Instructor/Revenue/index.jsx
+++ src/pages/Instructor/Revenue/index.jsx
@@ -4,6 +4,7 @@ import RevenueStatistics from "./components/RevenueStatistics/RevenueStatistics"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import RevenueChart from "./components/RevenueChart/RevenueChart";
 import RevenueSummary from "./components/RevenueSummary/RevenueSummary";
+import CoursesRevenue from "./components/CoursesRevenue/CoursesRevenue";
 
 const Revenue = () => {
   return (
@@ -11,7 +12,8 @@ const Revenue = () => {
       <RevenueStatistics />
       <Grid2 container spacing={3} mt="16px">
         <Grid2 xs={12} md={8}>
-          <RevenueChart />
+          {/* <RevenueChart /> */}
+          <CoursesRevenue />
         </Grid2>
         <Grid2 xs={12} md={4}>
           <RevenueSummary />
