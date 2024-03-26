4a7c811469e68ac608ceb081a4002b3900a0a874
Osama Safwat <rovendix@gmail.com>
2024-03-27 01:56:28 +0200

    added Time indicator and volume button to Video Player

--- src/features/VideoPlayer/index.tsx
+++ src/features/VideoPlayer/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "@vidstack/react/player/styles/base.css";
 import "@vidstack/react/player/styles/plyr/theme.css";
 import "./style.css";
@@ -7,10 +8,14 @@ import {
   plyrLayoutIcons,
 } from "@vidstack/react/player/layouts/plyr";
 
+import VolumeButton from "./Components/VolumeButton/index.tsx";
+import TimeIndicator from "./Components/TimeIndicator/index.tsx";
+
 export default function VideoPlayer({
   title,
   src,
   poster,
+  alt,
   subtitles,
   thumbnails,
 }) {
@@ -21,25 +26,19 @@ export default function VideoPlayer({
           borderRadius: "0px",
         }}
       />
-      <Poster src={poster} />
+      <Poster src={poster} alt={alt || "video poster"} />
       <PlyrLayout
         thumbnails={thumbnails}
-        icons={plyrLayoutIcons}
+        icons={{ ...plyrLayoutIcons }}
         style={{
           borderRadius: "0px",
         }}
         invertTime={false}
-        controls={[
-          "play-large",
-          "play",
-          "progress",
-          "current-time",
-          "duration",
-          "settings",
-          "mute",
-          // "volume",
-          "fullscreen",
-        ]}
+        slots={{
+          afterPlayButton: <TimeIndicator />,
+          afterSettings: <VolumeButton />,
+        }}
+        controls={["play-large", "play", "progress", "settings", "fullscreen"]}
       />
       {subtitles?.map((subtitle) => (
         <Track
