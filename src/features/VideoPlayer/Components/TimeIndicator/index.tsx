2f21fe9d09049a09f2a77e1a774963fdfe83bef9
Osama Safwat <rovendix@gmail.com>
2024-03-27 17:52:12 +0200

    disabled video from playing on full screen on mobile devices by default

--- src/features/VideoPlayer/Components/TimeIndicator/index.tsx
+++ src/features/VideoPlayer/Components/TimeIndicator/index.tsx
@@ -5,10 +5,16 @@ import React from "react";
 const TimeIndicator = () => {
   return (
     <Box display="flex" gap="4px" flex="1" justifyContent="center">
-      <Typography variant="body2" display="inline-block">
+      <Typography variant="body2" display="inline-block" component="div">
         <Time type="current" />
       </Typography>
-      <Typography variant="body2" display="flex" gap="2px" color="grey.400">
+      <Typography
+        variant="body2"
+        display="flex"
+        gap="2px"
+        color="grey.400"
+        component="div"
+      >
         / <Time type="duration" />
       </Typography>
     </Box>
