9fc790df793fae37810d13476df3bdd6916faf0f
Osama Safwat <rovendix@gmail.com>
2024-03-28 17:56:35 +0200

    added balance card to student billing and changed border color of transactions table

--- src/pages/Instructor/Revenue/components/RevenueSummary/RevenueSummary.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RevenueSummary = () => {
-  return <div>RevenueSummary</div>;
-};
-
-export default RevenueSummary;
--- src/pages/Student/Billing/index.jsx
+++ src/pages/Student/Billing/index.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import React from "react";
 import TransactionsTable from "./components/TransactionsTable";
+import BalanceCard from "./components/BalanceCard";
 
 const Billing = () => {
   return (
     <Box>
+      <BalanceCard />
       <TransactionsTable />
     </Box>
   );
