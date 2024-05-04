22893054b55e986f2b7999d2b890047f89aa74f4
salah samir <salahsaamir707@gmail.com>
2024-05-04 22:21:58 +0300

    Delete NavBar Component and design the search component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -29,29 +29,48 @@ useEffect(() => {
   <>
   
   <div className='mx-auto max-w-2xl px-4 py-3 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8'>
-    <div className='mt-2 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-    {data.map((ele) => (
-                  <div key={ele.id} className='group relative' onClick={()=>{nav(`/courseDetails/${ele._id}`)}}>
-                   
+    
+     {data.length > 0 ? (
+        <div className='my-3'>
+            <div className='mx-auto max-w-2xl p py-3  sm:py-16 lg:max-w-7xl'>
+              <div className='mt-2 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8'>
+                {data.map((ele) => (
+                  <div key={ele.id} className='group relative shadow-lg' onClick={()=>{nav(`/courseDetails/${ele._id}`)}}>
                     <div className='mt-2   '>
-                      
-                      <img src={ele.coverImageUrl} alt={ele.title} className='h-40 w-full object-cover object-center rounded-tr-3xl' />
-                     
-                      <div>
+                     <div className='relative'>
+                     <img src='https://cdn.pixabay.com/photo/2016/06/01/06/26/open-book-1428428_640.jpg' alt={ele.title} className='h-40 w-full object-cover object-center rounded-lg' />
+                     <div className='absolute bottom-2 right-2 bg-black opacity-60 p-1 rounded-md '> Best</div>
+                     </div>
+                      <div className='p-3'>
                         
-                          <Typography variant='p' py={4} color='primary'>{ele.title}</Typography>
+                         <div className="flex justify-between">
+                         <h6 className='text-1xl font-medium'>{ele.title}</h6>
+                          <div className='pt-1 px-2'> <Rating size='small' color='secondary'  name="read-only" value={ele.rating} readOnly /></div>
+                         </div>
                         
-                       <div className='flex pt-3 '>
-                       <Typography color='thrid' px-4 >{ele.rating}</Typography>
-                       <div className='pt-1 px-2'> <Rating size='small' color='secondary'  name="read-only" value={ele.rating} readOnly /></div>
-                       </div>
-
+                        <div className="flex justify-between my-3">
+                        <div className="flex">
+                          
+                          <img class="rounded-full w-10 h-10" src="https://cdn.pixabay.com/photo/2024/03/29/17/55/ai-generated-8663328_640.png" alt="image description"/>
+                          <p className='pt-1 px-2' style={{fontSize:'14px'}}>John Doe</p>
+                          
+                                                   </div>
+                               <h6 className='pt-1 px-2'>{ele.price} EGP</h6>
+                        </div>
                       </div>
-                      <Typography color='thrid' className='text-sm font-medium'>${ele.price}</Typography>
                     </div>
                   </div>
                 ))}
-    </div>
+              </div>
+            </div>
+          </div>
+      
+      ) : (
+        <div width={'100%'} height={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+          <span class='loader'></span>
+        </div>
+      )}
+   
   </div>
 
     
