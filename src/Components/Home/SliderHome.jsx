415d4b28b145c66dd7c99ee0d7f33f8ffd467df0
salah samir <salahsaamir707@gmail.com>
2024-06-30 22:10:00 +0300

    add  home image

--- src/Components/Home/SliderHome.jsx
+++ src/Components/Home/SliderHome.jsx
@@ -6,7 +6,7 @@ export default function SliderHome() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img
-        src="https://media.istockphoto.com/id/1575126031/photo/mid-aged-business-man-working-on-laptop-computer-in-office-writing-notes.jpg?s=612x612&w=0&k=20&c=kZVrVy-EV86bI26rb7bn8MRNkYtdP0uO08Ofe_GuoOg="
+        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
       />
